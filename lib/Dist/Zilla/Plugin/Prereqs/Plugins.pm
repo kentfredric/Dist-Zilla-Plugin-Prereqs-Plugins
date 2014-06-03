31d30523c0659ade21dcd4f98e5e9728b55babe3
Kent Fredric <kentfredric@gmail.com>
2014-06-03 19:49:00 +1200

    Build results of 9cb6798 (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/Plugins.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Plugins.pm
@@ -9,7 +9,7 @@ our $VERSION = '1.000000';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
-use Moose;
+use Moose qw( with has around );
 use MooseX::Types::Moose qw( HashRef ArrayRef Str );
 
 with 'Dist::Zilla::Role::PrereqSource';
