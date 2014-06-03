9cb6798ba34eadf6037079b8e1509cd19e363e82
Kent Fredric <kentfredric@gmail.com>
2014-06-03 19:48:53 +1200

    Subroutines::ProhibitCallsToUndeclaredSubs

--- lib/Dist/Zilla/Plugin/Prereqs/Plugins.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Plugins.pm
@@ -9,7 +9,7 @@ our $VERSION = '1.000000';
 
 # AUTHORITY
 
-use Moose;
+use Moose qw( with has around );
 use MooseX::Types::Moose qw( HashRef ArrayRef Str );
 
 with 'Dist::Zilla::Role::PrereqSource';
